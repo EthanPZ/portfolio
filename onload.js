eb2cb524035e31d5c31f0d3ceba3371cb50ea5f6
Ethan <ethanperetz000707@gmail.com>
2023-03-28 17:48:57 +0300

    updated & improved

---
+++ onload.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const loaderWrapper = document.querySelector(".loader");
+////////////////////////////////////////////////////////
+
+function loaderOut(opacity, overflow, display) {
+  loaderWrapper.style.opacity = opacity;
+  document.documentElement.style.overflow = overflow;
+
+  loaderWrapper.style.display = display;
+}
+
+loaderOut(undefined, "visible", "flex");
+
+setTimeout(() => {
+  loaderOut(0, "visible");
+}, 2000);
+
+setTimeout(() => loaderOut(undefined, undefined, "none"), 2200);
